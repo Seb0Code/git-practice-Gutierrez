651052865e7c0267430edbb9d4342cfe3632ed76
Seb0Code <sebastiangutierrezc7@gmail.com>
2026-05-09 21:00:58 -0400

    feat: agregar bucle for

--- holamundo.cpp
+++ holamundo.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
 using std::cin;
 using std::cout;
+using std::endl; // Implementacion del std::endl
 
 int main() {
     cout << "Hello World Desde Git\n";
     cout << "Mi nombre es Sebastian\n";
     cout << "Me gustaria saludar a Todos los presentes\n";
     cout << "Estoy viendo Programacion 2 :)\n";
+
+    // Bucle de 5 'Hola'
+    for (size_t e = 0; e < 5; e++) {
+        cout << "Hola" << endl;
+    }
     return 0;
 }
