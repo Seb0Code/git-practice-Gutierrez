0b7e358dd31357f01f642437a11c8de0c21b2665
Sebastian Gutierrez <sebastiangutierrezc7@gmail.com>
2026-05-09 20:52:13 -0400

    feat: agregar mas mensajes al programa

--- holamundo.cpp
+++ holamundo.cpp
@@ -5,5 +5,7 @@ using std::cout;
 int main() {
     cout << "Hello World Desde Git\n";
     cout << "Mi nombre es Sebastian\n";
+    cout << "Me gustaria saludar a Todos los presentes\n";
+    cout << "Estoy viendo Programacion 2 :)\n";
     return 0;
 }
